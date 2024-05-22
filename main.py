b4f36a1a7b5c7dff7db8dca2cbf3ea0943686437
Chaturasan Ramineni <chatur4686@gmail.com>
2024-05-22 19:20:59 +0530

    modified the port

--- main.py
+++ main.py
@@ -10,5 +10,5 @@ logging.basicConfig(
 if __name__ == "__main__":
     app = create_app()
     app.app_context().push()
-    app.run(host="0.0.0.0", port=8000)
+    app.run(host="0.0.0.0", port=5000)
     # app.run(debug=True)
